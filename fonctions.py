62e3a03d4b522856b420a0f9a2a7b89ec3db71b7
Cyriac <cyriac.thibaudeau@ens.uvsq.fr>
2023-05-10 16:42:07 +0200

    le push de légende: ajout du temps pour les pistes dans le nv json, implémentation clean dans fonctions de l'algo de dijkstra

--- fonctions.py
+++ fonctions.py
@@ -2,53 +2,58 @@ import json
 
 def dijkstra(graph, start, end, niveau):
     # Initialisation des structures de données
-    distances = {node['name']: float('inf') for node in graph['noeuds']}
-    distances[start] = 0
+    all_temps = {node['name']: float('inf') for node in graph['noeuds']}
+    all_temps[start] = 0
     visited = set()
     predecessors = {}
 
     # Boucle principale de l'algorithme
     while len(visited) != len(graph['noeuds']):
-        # Recherche du noeud non visité avec la plus petite distance
-        min_distance = float('inf')
+        # Recherche du noeud non visité avec le plus petite temps
+        min_temps = float('inf')
         min_node = None
         for node in graph['noeuds']:
-            if node['name'] not in visited and distances[node['name']] < min_distance:
-                min_distance = distances[node['name']]
+            if node['name'] not in visited and all_temps[node['name']] < min_temps:
+                min_temps = all_temps[node['name']]
                 min_node = node['name']
         if min_node is None:
             break
         visited.add(min_node)
 
-        # Mise à jour des distances pour chaque voisin du noeud courant
+        # Mise à jour de all_temps pour chaque voisin du noeud courant
         for edge in get_neighbors(graph, min_node):
             neighbor = edge['noeud_fin']
-            poids = edge['longueur']
+            poids = edge['temps']
             couleur = edge['couleur']
 
-            if niveau == "débutant":
-                if couleur == "blue":
-                    distance = min_distance + poids*1.5
-                elif couleur == "red":
-                    distance = min_distance + poids*2
-                elif couleur == "black":
-                    distance = min_distance + poids*4
-                else:
-                    distance = min_distance + poids
-                
-            elif niveau == "moyen":
-                if couleur == "red":
-                    distance = min_distance + poids*1.5
-                elif couleur == "black":
-                    distance = min_distance + poids*2
-                else:
-                    distance = min_distance + poids
-
+            if couleur in ["green", "blue", "red", "black"]:
+                if niveau == "débutant":
+                    if couleur == "green":
+                        temps = min_temps + poids
+                    elif couleur == "blue":
+                        temps = min_temps + poids*1.5
+                    elif couleur == "red":
+                        temps = min_temps + poids*2
+                    elif couleur == "black":
+                        temps = min_temps + poids*4
+                      
+                elif niveau == "moyen":
+                    if couleur == "green":
+                        temps = min_temps + poids
+                    elif couleur == "blue":
+                        temps = min_temps + poids
+                    elif couleur == "red":
+                        temps = min_temps + poids*1.5
+                    elif couleur == "black":
+                        temps = min_temps + poids*2
+                    
+                elif niveau == "avance":
+                    temps = min_temps + poids
             else:
-                distance = min_distance + poids
+                temps = min_temps + poids
 
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
+            if temps < all_temps[neighbor]:
+                all_temps[neighbor] = temps
                 predecessors[neighbor] = min_node
 
     # Construction du chemin le plus court en remontant les prédecesseurs
@@ -59,7 +64,7 @@ def dijkstra(graph, start, end, niveau):
         node = predecessors[node]
     path.insert(0, start)
 
-    return path, distances[end]
+    return path, all_temps[end]
 
 def get_neighbors(graph, node):
     # Retourne la liste des voisins d'un noeud dans le graphe
@@ -70,12 +75,12 @@ def get_neighbors(graph, node):
     return neighbors
 
 # Lecture du fichier JSON représentant le graphe
-with open('data\courchevel.json') as f:
+with open('courchevelwithspeed.json') as f:
     graph = json.load(f)
 
 # Exécution de l'algorithme de Dijkstra pour trouver le plus court chemin entre deux points
-path, distance = dijkstra(graph, 'bas GRANGETTES', 'COL DE CHANROSSA', 'débutant')
+path, temps = dijkstra(graph, 'bas VERDONS', 'n90', 'avance')
 
 # Affichage du résultat
 print('Le plus court chemin est :', ' -> '.join(path)) #path est une liste
-print('La distance totale est :', distance) #distance correspond à la distance totale entre les deux noeuds
+print('Le temps total est :', temps, "secondes") #temps correspond au temps total entre les deux noeuds
